0826ce942627d424d5044c3431149a60693572ca
THSCOMPANY <97855325+THSCOMPANY@users.noreply.github.com>
2022-01-16 21:03:21 -0300

    Add files via upload

---
+++ Vendas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        public void inserirProdutos()
+        {
+
+        }
+
+        public static void retirarProdutos()
+        {
+
+        }
+	}
+}
